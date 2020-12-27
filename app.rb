461c9221ce566d73ba0d0b9aae26ae7d1a3ec3a1
umapathireddy <35969031+umapathireddy@users.noreply.github.com>
2020-12-27 12:32:41 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ File.readlines("data/HAL_quotes.txt").each {|quote| QUOTES.push(quote)}
 # Controllers
 
 get '/*' do
-  @greeting="Hello world!"
+  @greeting="Savvas!"
   @quote="\"#{QUOTES.sample.strip}\""
   erb :index
 end
